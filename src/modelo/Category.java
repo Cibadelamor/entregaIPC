a80d441b6af768bd37764ad8345080e4b5661d27
esteb <esteb@AORUS>
2024-05-02 13:53:09 +0200

    Añadido el constructor general a la clase Category

--- src/modelo/Category.java
+++ src/modelo/Category.java
@@ -11,4 +11,9 @@ package modelo;
 public class Category {
     private String name;
     private String description;
+    
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 }
